41ac9bef028df790d9e365994e6a720a73062b6c
Babatunde Isaac Oluwaseun <babatundeisaac94@gmail.com>
2023-11-13 05:19:41 -0500

    yarn and port

--- myApp.js
+++ myApp.js
@@ -1,53 +1,6 @@
 const express = require('express');
 const helmet = require('helmet')
 const app = express();
-const PORT = process.env.PORT || 3030
-
-app.listen(PORT, () => {
-  console.log(`Infosec App Started on${PORT}`)
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -61,7 +14,7 @@ app.use('/_api', api);
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
-let port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
+let PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Infosec App Started on ${PORT}`)
 });
