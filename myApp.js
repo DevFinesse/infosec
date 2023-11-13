b3b7341de40d93f2b79f01fc1eb77672e9d9ab5f
Babatunde Isaac Oluwaseun <babatundeisaac94@gmail.com>
2023-11-13 06:34:53 -0500

    added https access only

--- myApp.js
+++ myApp.js
@@ -11,6 +11,9 @@ app.use(helmet.xssFilter({}))
 app.use(helmet.noSniff())
 //Prevent IE from Opening Untrusted HTML with helmet.ieNoOpen()
 app.use(helmet.ieNoOpen())
+//Ask Browsers to Access Your Site via HTTPS Only with helmet.hsts()
+let ninetyDaysInSeconds = 90*24*60*60
+app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}))
 
 
 
