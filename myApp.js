8d511060973bae81316e430d16b326cae64180ab
Babatunde Isaac Oluwaseun <babatundeisaac94@gmail.com>
2023-11-13 06:46:06 -0500

    disable client side caching

--- myApp.js
+++ myApp.js
@@ -16,6 +16,8 @@ let ninetyDaysInSeconds = 90*24*60*60
 app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}))
 //Disable DNS Prefetching with helmet.dnsPrefetchControl()
 app.use(helmet.dnsPrefetchControl())
+//Disable Client-Side Caching with helmet.noCache()
+app.use(helmet.noCache())
 
 
 
